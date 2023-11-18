873284f3d2958fdc3fa07240488f85b0006fe943
Yan Vargas <120762798+yanvargas@users.noreply.github.com>
2023-11-17 23:41:26 -0300

    Create Disciplina.java

---
+++ Disciplina.java
@@ -0,0 +1,49 @@
+package Entrega;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Disciplina {
+    private String nome;
+    private List<Aluno> alunos;
+    private List<Curso> cursos;
+    private Professor professores;
+
+    public Disciplina(String nome) {
+        this.nome = nome;
+        this.alunos = new ArrayList<>();
+        this.cursos = new ArrayList<>();;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public List<Aluno> getAlunos() {
+        return alunos;
+    }
+
+    public List<Curso> getCursos() {
+        return cursos;
+    }
+
+    public List<Professor> getProfessores() {
+        return professores;
+    }
+
+      public void setNome(String nome) {
+        this.nome=nome;
+    }
+
+    public void addAluno(Aluno aluno) {
+        alunos.add(aluno);
+    }
+
+    public void addCurso(Curso curso) {
+        cursos.add(curso);
+    }
+  
+    public void addProfessor(Professor professor) {
+        this.pessoas=pessoas;
+    }
+}
